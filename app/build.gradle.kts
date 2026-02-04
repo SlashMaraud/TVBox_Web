a6b0d9b90c4299ded20e402cbf605752709b200f
SlashMaraud <163646802+SlashMaraud@users.noreply.github.com>
2026-02-04 19:27:22 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "dev.deads.webapp"
-        minSdk = 21
+        minSdk = 21 // GeckoView requiere mínimo 21, perfecto para Chromecast
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -30,6 +30,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     
-    // ESTA VERSIÓN ES LA QUE FUNCIONA: Versión estable oficial
-    implementation("org.mozilla.geckoview:geckoview-arm64-v8a:121.0")
+    // Esta es la versión "comodín" que Mozilla sirve por defecto.
+    // Si esta no la encuentra, es que hay un problema de conexión con su servidor.
+    implementation("org.mozilla.geckoview:geckoview-omni:115.0.20230710155057")
 }
