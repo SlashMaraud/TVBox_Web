1d3fe566e5f8f4c5535bb68de36d379ccc0697b0
SlashMaraud <163646802+SlashMaraud@users.noreply.github.com>
2026-02-04 20:10:27 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,13 @@ android {
         versionName = "1.0"
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -22,6 +29,10 @@ android {
 
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    
+    buildFeatures {
+        compose = false
     }
 }
 
