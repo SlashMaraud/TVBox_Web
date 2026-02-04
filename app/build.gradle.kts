fd3c5efd8548539295c357110a13f9b5e64621be
SlashMaraud <163646802+SlashMaraud@users.noreply.github.com>
2026-02-04 20:45:12 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "dev.deads.webapp"
+        applicationId = "dev.deads.hdfull"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
